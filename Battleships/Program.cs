80a046687de6fbadf251bffea75907ed3e22bd1e
Christopher Kelly-Brown <ckellybrown@outlook.com>
2021-10-20 15:15:48 +0100

    SetupGame Method

--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -14,15 +14,11 @@ namespace Battleships
                 {
                     playerOnePlot[x,y] = '~';
                 }
-                
             }
 
             DisplayBoard(playerOnePlot);
             Console.WriteLine("\nPlayer one Enter first location of destroyer (5)");
-            string coord = Console.ReadLine(); // A1
-            int letterInt = char.ToUpper(coord[0]) - 64 - 1; // ABC to 012
-            Console.WriteLine((coord[1]-1)+""+ letterInt);
-            playerOnePlot[(Int32.Parse(Convert.ToString(coord[1]))-1), letterInt] = 'S';
+            SetupGame(playerOnePlot, 3);
             DisplayBoard(playerOnePlot);
 
 
@@ -43,6 +39,43 @@ namespace Battleships
                 }
                 Console.Write("\n");
             }
+        }
+
+        static char[,] SetupGame(char[,] board, int shipLength)
+        {
+            string coord = Console.ReadLine(); // A1
+            int x = (Int16.Parse(Convert.ToString(coord[1])) - 1);
+            int y = char.ToUpper(coord[0]) - 64 - 1; // ABC to 012
+            Console.WriteLine(x + "" + y);
+            Console.Write("Which direction should the battleship point? (N/E/S/W) ");
+            char dir = char.ToLower(Console.ReadKey().KeyChar);
+            int dirInt;
+            Console.Write("\n");
+            int xFact = 0;
+            int yFact = 0;
+            if (dir == 'n')
+            {
+                yFact = -1;
+            }
+            if (dir == 's')
+            {
+                yFact = 1;
+            }
+            if (dir == 'e')
+            {
+                xFact = -1;
+            }
+            if (dir == 'w')
+            {
+                xFact = 1;
+            }
+
+            for (int i = 0; i < shipLength; i++)
+            {
+                board[x, y] = 'X';
+            }
+
+            return board;
         }
     }
 }
